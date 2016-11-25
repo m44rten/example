c42f87c80b6c1a75f0f3f6cdbf2639d3387882c0
Prelang Builder <builder@prelang.com>
2016-11-25 16:13:21 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Personen' to allow for create, update

--- app/admin/personen.rb
+++ app/admin/personen.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Personen do
+  permit_params :voornaam, :achternaam, :email, :telefoon, :mobiel, :tussenvoegsel, :type, :functie, :afdeling
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
