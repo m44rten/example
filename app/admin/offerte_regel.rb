9051c05b9dec08ece7fbd690d49d8bdb1dcc0704
Prelang Builder <builder@prelang.com>
2016-11-25 16:13:06 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'OfferteRegel' to allow for create, update

--- app/admin/offerte_regel.rb
+++ app/admin/offerte_regel.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register OfferteRegel do
+  permit_params :artikel, :omschrijving, :aantal, :prijs, :post
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
